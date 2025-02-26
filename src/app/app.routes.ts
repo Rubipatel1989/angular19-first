46a1731494366af02930b22a465eaaa601825293
Pawan Kumar <pawank@exeire.com>
2025-02-26 10:24:11 +0530

    remove extra code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './components/about/about.component';
 export const routes: Routes = [
     
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'about/:id',
-        component: AboutComponent
-    }
+   
 ];
 
 
