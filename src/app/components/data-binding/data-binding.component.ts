4cab82147bd134a3840e301e2b6e32ea3bca2f3c
Pawan Kumar <pawank@exeire.com>
2025-02-27 09:19:04 +0530

    One Way Data Binding Finished

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
-courseName: string = 'Angular 19';
-courseDuration: string = '3 months';
+  courseName: string = 'Angular 19';
+  courseDuration: string = '3 months';
+  clsName = 'col-3';
+  placeholder = 'Enter course name';
+
+  showAlert(){
+    alert('Alert from data binding');
+  }
+  showMessage(message: string){
+    alert(message);
+  }
+ 
 }
