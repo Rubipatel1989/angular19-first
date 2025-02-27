e7874868fcead6c82d2a546eef6f90c9ad8fb3a0
Pawan Kumar <pawank@exeire.com>
2025-02-27 07:21:03 +0530

    Data Variables Done

--- src/app/components/variables/variables.component.ts
+++ src/app/components/variables/variables.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './variables.component.css'
 })
 export class VariablesComponent {
+  firstName: string = 'Pawan Kumar';
+  lastName: string = 'Gupta';
+  Age: number = 25;
+  Address: string = 'Noida';
+  Wife:string;
+  currentDate: Date = new Date();
+  isActive:Boolean = true;
+  Student:any;
 
+  constructor(){
+    this.Wife = 'Neha';
+    this.Student = {
+      name: 'Manoj',
+      age: 30
+    }
+  }
 }
