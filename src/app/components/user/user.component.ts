adbb13709ea40d62f0194d9e5f706ed10ab66b88
Pawan Kumar <pawank@exeire.com>
2025-03-03 16:55:21 +0530

    Add User List Page

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrls: ['./user.component.css']
 })
 export class UserComponent {
+  @ViewChild('userFormPanel', { static: false }) formPanel!: ElementRef;
 
+  toggleForm() {
+    if (this.formPanel) {
+      const panel = this.formPanel.nativeElement;
+      panel.style.display = (panel.style.display === 'none' || panel.style.display === '') ? 'block' : 'none';
+    }
+  }
 }
