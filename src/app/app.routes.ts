868864ae333367f94853c2414990142d4ee9f690
Pawan Kumar <pawank@exeire.com>
2025-03-03 15:38:28 +0530

    User Component Created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { VariablesComponent } from './components/variables/variables.component';
 import { DirectiveComponent } from './components/directive/directive.component';
 import { TemplateFormComponent } from './components/template-form/template-form.component';
 import { ApiCallComponent } from './components/api-call/api-call.component';
+import { UserComponent } from './components/user/user.component';
 
 export const routes: Routes = [
     {
@@ -20,7 +21,8 @@ export const routes: Routes = [
             { path: 'data-binding', component: DataBindingComponent },
             { path: 'variables', component: VariablesComponent },
             {path:'template-form', component: TemplateFormComponent},
-            {path:'api-call', component:ApiCallComponent}
+            {path:'api-call', component:ApiCallComponent},
+            {path:'user', component:UserComponent}
 
         ]
     }
--- src/app/components/api-call/api-call.component.ts
+++ src/app/components/api-call/api-call.component.ts
@@ -11,11 +11,23 @@ import { FormsModule } from '@angular/forms';
 })
 export class ApiCallComponent {
   http = inject(HttpClient);
-  jsonUsers: any [] = [];
+  jsonUsers: any[] = [];
+  jsonAllUsers: any[] = [];
   getAllUsers() {
     this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((response: any) => {
       console.log(response);
       this.jsonUsers = response;
+    });
+  }
+  getComplainetUsers() {
+    this.http.get('https://projectapi.gerasim.in/api/Complaint/GetAllUsers').subscribe((response: any) => {
+     // console.log(response);
+      if (response.result) {
+        console.log(response.data);
+        this.jsonAllUsers = response.data;
+      } else {
+        alert(response.message);
+      }
     });
   }
 }
