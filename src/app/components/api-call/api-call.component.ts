bb133717cf234d7d82b082c95c8503d24d8ef3a0
Pawan Kumar <pawank@exeire.com>
2025-03-03 15:25:33 +0530

    Json Placeholder API Call

--- src/app/components/api-call/api-call.component.ts
+++ src/app/components/api-call/api-call.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-api-call',
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './api-call.component.html',
   styleUrl: './api-call.component.css'
 })
 export class ApiCallComponent {
-
+  http = inject(HttpClient);
+  jsonUsers: any [] = [];
+  getAllUsers() {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((response: any) => {
+      console.log(response);
+      this.jsonUsers = response;
+    });
+  }
 }
