8b0274928a524455a45dd467407e2127a3c39d38
Pawan Kumar <pawank@exeire.com>
2025-03-04 16:01:29 +0530

    Add Loader

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,25 +1,30 @@
+import { NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [NgIf],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
   http = inject(HttpClient);
   userList : any []= [];
+  isLoading: boolean = false;
 
   constructor(){
     this.getUsers();
   }
   getUsers(){ 
+    this.isLoading = true;
     this.http.get('https://projectapi.gerasim.in/api/Complaint/GetAllUsers').subscribe((response: any) => {
       console.log(response);
       if (response.result) {
         console.log(response.data);
         this.userList = response.data;
+        this.isLoading = false;
       } else {
         alert(response.message);
       }
