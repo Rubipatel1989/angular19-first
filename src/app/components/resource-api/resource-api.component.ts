d64b1e4ac3dbd60f31e92a635780fa5adc85f64a
Pawan Kumar <pawank@exeire.com>
2025-03-10 11:56:22 +0530

    resource & observaable

--- src/app/components/resource-api/resource-api.component.ts
+++ src/app/components/resource-api/resource-api.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, resource } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-resource-api',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './resource-api.component.html',
   styleUrl: './resource-api.component.css'
 })
 export class ResourceApiComponent {
+  http = inject(HttpClient);
+
+  userListResource = resource({
+    loader: () => {
+      return fetch('https://jsonplaceholder.typicode.com/users')
+        .then((res) => res.json() as Promise<any[]>)
+    }
+  });
+
+  userList = rxResource({
+    loader: () => {
+      return this.http.get<any[]>('https://jsonplaceholder.typicode.com/users');
+    }
+});
 
 }
