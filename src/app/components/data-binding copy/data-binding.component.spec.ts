f053530be9db9b4a64d85b3dac4894975d4ff628
Pawan Kumar <pawank@exeire.com>
2025-03-16 08:10:10 +0530

    Files

--- src/app/components/data-binding copy/data-binding.component.spec.ts
+++ src/app/components/data-binding copy/data-binding.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ApiCallComponent } from './api-call.component';
+import { DataBindingComponent } from './data-binding.component';
 
-describe('ApiCallComponent', () => {
-  let component: ApiCallComponent;
-  let fixture: ComponentFixture<ApiCallComponent>;
+describe('DataBindingComponent', () => {
+  let component: DataBindingComponent;
+  let fixture: ComponentFixture<DataBindingComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ApiCallComponent]
+      imports: [DataBindingComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ApiCallComponent);
+    fixture = TestBed.createComponent(DataBindingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
